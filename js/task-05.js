262e11fe330c1a8c29379e331f60ba6d45fe8c9b
Dmitriy Panivnyk <d.panivnyk@ukr.net>
2022-07-27 09:44:54 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,8 +1,14 @@
 const nameInput = document.querySelector("#name-input");
 const nameOutput = document.querySelector("#name-output");
 
+// nameInput.addEventListener("input", (event) => {
+//   event.target.value
+//     ? (nameOutput.textContent = event.currentTarget.value)
+//     : (nameOutput.textContent = "Anonymous");
+// });
+
 nameInput.addEventListener("input", (event) => {
-  event.target.value
-    ? (nameOutput.textContent = event.currentTarget.value)
-    : (nameOutput.textContent = "Anonymous");
+  if (event.target.value) {
+    nameOutput.textContent = event.currentTarget.value;
+  } else nameOutput.textContent = "Anonymous";
 });
